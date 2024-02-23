a193240ad24d16a117f8feea8d9c9437f229389d
YoujunZhang <144183143+YoujunZhang@users.noreply.github.com>
2024-02-22 19:25:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function mouseDown() {
+    document.getElementById("one").style.color = "transparent";
+    document.getElementById("one").style.textShadow = "0 0 3px rgb(202, 177, 35)";
+
+  }
+  
+  function mouseUp() {
+    document.getElementById("one").style.color = "gold";
+    document.getElementById("one").style.opacity = 0.55;
+  }
+
+
+
+
+function myFunction(element, clr) {
+    element.style.color = clr;
+}
+
+
+
+
+function myFunction() {
+  location.replace("https://youjunzhang.github.io/Interaction-Studio-Spring-Project-1-Page-2-1/")
+}
